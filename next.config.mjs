26531044ceafded7aad2f5970bcc9862320bcccf
Narotam <112194430+NarotamOfficial@users.noreply.github.com>
2026-05-21 09:18:06 +0530

    Add initial Next.js configuration

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  poweredByHeader: false,
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true }
+};
+
+export default nextConfig;
+
